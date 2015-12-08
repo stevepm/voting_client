bb00407b7c214745bd2e06ac073a997b50a9c417
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-12-07 22:31:46 -0800

    send vote function from actionCreators as prop to Voting component

--- src/index.jsx
+++ src/index.jsx
@@ -11,17 +11,16 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch(setState(state));
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
-        store.dispatch({type: 'SET_STATE', state})
+        store.dispatch(setState(state))
 );
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer}/>
   <Route path="/" component={VotingContainer}/>
-</Route>
+</Route>;
 
 ReactDOM.render(
     <Provider store={store}>
