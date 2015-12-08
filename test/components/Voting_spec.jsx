70c94a1640f25104c1cc7a4b2d163eabf20fe9fb
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-12-07 20:40:55 -0800

    use pure render mixin

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react/addons';
+import {List} from 'immutable';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -53,7 +54,7 @@ describe('Voting', () => {
   it('renders just the winner when there is one', () => {
     const component = renderIntoDocument(
         <Voting pair={['train', 'a']}
-                winner='train' />
+                winner='train'/>
     );
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
     expect(buttons.length).to.equal(0);
@@ -61,6 +62,36 @@ describe('Voting', () => {
     const winner = React.findDOMNode(component.refs.winner);
     expect(winner).to.be.ok;
     expect(winner.textContent).to.contain('train');
+  });
+
+  it('renders as a pure component', () => {
+    const pair = ['trainspotting', '28'];
+    const component = renderIntoDocument(
+        <Voting pair={pair}/>
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('trainspotting');
+
+    pair[0] = 'sunshine';
+    component.setProps({pair: pair});
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('trainspotting');
+  });
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('trainspotting', '28');
+    const component = renderIntoDocument(
+        <Voting pair={pair} />
+    );
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('trainspotting');
+
+    const newPair = pair.set(0, 'sunshine');
+    component.setProps({pair: newPair});
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('sunshine');
   })
 
 });
